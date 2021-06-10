8de104bd81fe15afcf63c487cd9245aed2fa006a
roman rodriguez <roman.rodriguez@unosquare.com>
2021-06-10 13:43:18 -0500

    initial terraform version

---
+++ terraform-gke/variables.tf
@@ -0,0 +1,39 @@
+variable "project_id" {
+  description = "The project ID to host the cluster in"
+  default = "testkubernetesterraform"
+}
+
+variable "zone" {
+  description = "Google Cloud zone"
+  type        = string
+  default     = "us-central1-c"
+}
+
+variable "cluster_name" {
+  description = "The name for the GKE cluster"
+  default     = "terraformk8s-cluster"
+}
+variable "env_name" {
+  description = "The environment for the GKE cluster"
+  default     = "prod"
+}
+variable "region" {
+  description = "The region to host the cluster in"
+  default     = "us-central1"
+}
+variable "network" {
+  description = "The VPC network created to host the cluster in"
+  default     = "gke-network"
+}
+variable "subnetwork" {
+  description = "The subnetwork created to host the cluster in"
+  default     = "gke-subnet"
+}
+variable "ip_range_pods_name" {
+  description = "The secondary ip range to use for pods"
+  default     = "ip-range-pods"
+}
+variable "ip_range_services_name" {
+  description = "The secondary ip range to use for services"
+  default     = "ip-range-services"
+}
